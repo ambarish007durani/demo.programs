a3a32ca685eedfb4e7030cf2a3538afdf84e1b65
ambarish007durani <105291472+ambarish007durani@users.noreply.github.com>
2022-05-14 18:57:07 +0530

    Add files via upload

--- Java_Demo/Addition.java
+++ Java_Demo/Addition.java
@@ -1,1 +1,36 @@
+/* Program to display hello message
+for the user*/
+
+//Program to display hello message
+import java.io.*;
+class Addition{
+	public static void main(String args[]){
+	int a=10;
+	int b=20;
+	int c;
+	c=a+b;
+	System.out.println("Addition is : " + c);
+	}
+}
+
+
+//Assignments:
+1. Write a program to addition of 4 numbers, substraction of 2 numbers, 
+multiplication of 5 numbers and division of 2 numbers and print the results 
+in different variables
+
+2. Write a program to calculate the percentage of your marks. 
+Takes any 5 subjects out of 100
+
+3. Write a program to print the following values on new lines,
+- First Name
+- Last Name
+- Age
+- Address
+- Gender 
+
+
+
+
+
 
